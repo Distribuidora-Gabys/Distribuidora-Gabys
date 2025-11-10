b10a1be739b1e13a2e1ebf0fa4e6245385841120
adriangeb28 <adriangeb2809@gmail.com>
2025-11-10 11:47:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-@app.route("/productos")
-def productos():
-    if "usuario" not in session:
-        return redirect(url_for("login"))
-    productos = cargar_json("productos.json")
 
-    query = request.args.get("q", "").strip().lower()
-    tipo_filtro = request.args.get("tipo", "").strip().lower()
-    if query or tipo_filtro:
-        productos = [
-            p for p in productos
-            if (not query or query in p["nombre"].lower() or query in p["codigo"].lower())
-            and (not tipo_filtro or p["tipo"].lower() == tipo_filtro)
-        ]
-
-    return render_template("productos.html", productos=productos)
